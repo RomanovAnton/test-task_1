8c3c19b248ab4bf9f8b4d8bb355ae84d047e1957
Anton Romanov <rad250495@yandex.ru>
2022-07-05 21:53:45 +0700

    deploy

--- src/components/DataSize/DataSize.js
+++ src/components/DataSize/DataSize.js
@@ -2,10 +2,10 @@ import { Button } from "react-bootstrap";
 import "./DataSize.css";
 function DataSize(props) {
   const urlSmall =
-    "https://www.filltext.com/?rows=32&id={number|1000}&firstName={firstName}&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}";
+    "http://www.filltext.com/?rows=32&id={number|1000}&firstName={firstName}&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}";
 
   const urlBig =
-    "https://www.filltext.com/?rows=1000&id={number|1000}&firstName={firstName}&delay=3&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}";
+    "http://www.filltext.com/?rows=1000&id={number|1000}&firstName={firstName}&delay=3&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}";
 
   return (
     <div className="box-center btn-box">
